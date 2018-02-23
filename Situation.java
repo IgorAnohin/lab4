47348611981132ba4c4465d6067480a17d6478bf
Igor Anokhin <igor.anohin@emlid.com>
2018-02-23 21:12:53 +0300

    Situation: rewrite methods
    Now methods more nicely
    swap ArraList on LinkedList
    Delete very big functions and add few small functions

--- Situation.java
+++ Situation.java
@@ -1,35 +1,48 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Random;
 
-class SilentHill extends Exception{
-
-	SilentHill(String name) {
-		super(name);
-		System.out.println("С людьми в городе что-то не так");
-	}
-
-}
-
-
-public class Situation
+class Situation
 {
 	static long passager_number = Math.round(Math.random()*4 + 1);
 	static long room_count = Math.round(Math.random()*5 + 5);
 	static long device_count = Math.round(Math.random()*room_count);
 
-	static Space_town town;
-	static Rocket Rockkket;
+	static Rocket rocket;
 
-	static ArrayList<Rocket_passager> Rocket_passageres = new ArrayList<>();
-	static ArrayList<Room> Rooms = new ArrayList<>();
-	static ArrayList<Device> Devices = new ArrayList<>();
+	static LinkedList<Rocket_passager> Rocket_passageres = new LinkedList<>();
+	static LinkedList<Room> Rooms = new LinkedList<>();
+	static LinkedList<Device> Devices = new LinkedList<>();
 
 
-	public static void main(String args[])
+	public Situation(String path_to_passagers, String path_to_rooms)
 	{
+		rocket = initialize(path_to_passagers, path_to_rooms);
+		print_passageres();
+		start_simulation();
+	}
+	public Situation()
+	{
+		rocket = initialize();
+		print_passageres();
+		start_simulation();
+	}
+
+
+	public static void print_passageres()
+	{
+		LinkedList<Rocket_passager> list = rocket.getRocket_passageres();
+		for (Rocket_passager  passager: list) {
+			System.out.println("Passager: "+ passager.getName() + " " + passager.getKnowledge() +
+					" " + passager.getPlace());
+		}
+	}
+
+	public static void start_simulation()
+	{
+
 		//Anonim class add
 		Bottom start = new Bottom(
-				new Power_interface()
-				{
+				new Power_interface() {
 					@Override
 					public void Set_power() {
 						System.out.println("Это ракета например(анонимная ракета между прочим)");
@@ -37,161 +50,228 @@ public class Situation
 						System.out.println();
 					}
 				}
-
 		);
 
 		start.TouchBottom();
 
-		for (int i = 0; i < passager_number; i++)
+		for (int current_passager = 0; current_passager < passager_number; current_passager++)
 		{
-			//Local exeption Class
-			class nadoelo_xodit extends NullPointerException {
-				private Room stop_room;
-				nadoelo_xodit(Room stop_room)
-				{
-					super();
-					this.stop_room = stop_room;
-				}
-
-				public void where_stop()
-				{
-					System.out.printf("Путь закончился в локации: %s\n",stop_room.getPlaceName() );
-				}
-			}
-
-			//Do nothing if passanger sleep
-			if (Rocket_passageres.get(i).getStatus() == Status.SLEEP) {
-				System.out.println(Rocket_passageres.get(i).getName() + " спит и некуда не пойдёт");
-				continue;
-			}
-
-			System.out.printf("Затаив дыхание, подопытный номер %s отправляется в свой нелегкий путь.\n",
-					Rocket_passageres.get(i).getName());
-			System.out.printf("Текущее местоположение: %s\n\n", Rocket_passageres.get(i).getPlace().getPlaceName());
-			try
-			{
-				while (true)
-				{
-					if (Rocket_passageres.get(i).getPlace().getNextRoom() == null) throw new nadoelo_xodit(Rocket_passageres.get(i).getPlace());
-
-					else
-						//if Knowledge Bad have chance stay this some time
-						if (Rocket_passageres.get(i).getRocketKnowledge() == RocketKnowledge.BAD)
-					    {
-					    	//While can't go from this room
-							while (!Rocket_passageres.get(i).getPlace().go_out()) {
-								Rocket_passageres.get(i).setStatus(Status.NERVOUS);
-								System.out.printf("%s не может найти двери и обходить комнату вокруг\n", Rocket_passageres.get(i).getName());
-
-								System.out.printf("%s теряет самообладание. Опять...\n", Rocket_passageres.get(i).getName());
-							}
-							if (Rocket_passageres.get(i).getPlace().getChance_go_out() != 1)
-								System.out.printf("%s наконец-то находит дверь\n", Rocket_passageres.get(i).getName());
-						}
-						//up_stairs if room have stairs
-						Rocket_passageres.get(i).up_stairs();
-						//activity with Bottom in Room
-						Rocket_passageres.get(i).getPlace().room_activity(Rocket_passageres.get(i));
-						if (Rocket_passageres.get(i).getPlace().getNextRoom() == null) throw new nadoelo_xodit(Rocket_passageres.get(i).getPlace());
-
-						//GO to the next room
-						Rocket_passageres.get(i).move();
-						System.out.println();
-					}
-			} catch (nadoelo_xodit e) //Если некуда идти
-			{
-				e.where_stop();
-				System.out.println("Комнат, куда можно пойти, больше нет. А возвращаться назад неохото...");
-				System.out.println("Посмотрим, пойдёт ли куда-нибудь кто-то ещё");
-				System.out.println();
-			}
+            //Do nothing if passanger sleep
+            if (Rocket_passageres.get(current_passager).getStatus() == Status.SLEEP) {
+                System.out.println(Rocket_passageres.get(current_passager).getName() + " спит и некуда не пойдёт");
+            } else {
+                try {
+                    walk(current_passager);
+                } catch (nadoelo_walk e) //Если некуда идти
+                {
+                    e.where_stop();
+                    System.out.println("Комнат, куда можно пойти, больше нет. А возвращаться назад неохото...");
+                    System.out.println("Посмотрим, пойдёт ли куда-нибудь кто-то ещё");
+                    System.out.println();
+                }
+            }
 		}
-
 	}
 
-	static {
+	private static void walk(int passager) throws nadoelo_walk
+    {
+        System.out.printf("Затаив дыхание, подопытный номер %s отправляется в свой нелегкий путь.\n",
+                Rocket_passageres.get(passager).getName());
+        System.out.printf("Текущее местоположение: %s\n\n", Rocket_passageres.get(passager).getPlace().getPlaceName());
+
+        while (true) {
+            if (Rocket_passageres.get(passager).getPlace().getNextRoom() == null)
+                throw new nadoelo_walk(Rocket_passageres.get(passager).getPlace());
+
+            //if Knowledge Bad have chance stay this some time
+            if (Rocket_passageres.get(passager).getKnowledge() == RocketKnowledge.BAD) {
+                //While can't go from this room
+                find_output(passager);
+            }
+
+            room_activities(passager);
+
+            //GO to the next room
+            Rocket_passageres.get(passager).move();
+            System.out.println();
+        }
+
+    }
+
+    private static void find_output(int passager)
+    {
+        while (!Rocket_passageres.get(passager).getPlace().go_out()) {
+            Rocket_passageres.get(passager).setStatus(Status.NERVOUS);
+            System.out.printf("%s не может найти двери и обходить комнату вокруг\n", Rocket_passageres.get(passager).getName());
+
+            System.out.printf("%s теряет самообладание. Опять...\n", Rocket_passageres.get(passager).getName());
+        }
+        if (Rocket_passageres.get(passager).getPlace().getChance_go_out() != 1)
+            System.out.printf("%s наконец-то находит дверь\n", Rocket_passageres.get(passager).getName());
+    }
+
+    private static void room_activities(int passager)
+    {
+        //up_stairs if room have stairs
+        Rocket_passageres.get(passager).up_stairs();
+        //activity with Bottom in Room
+        Rocket_passageres.get(passager).getPlace().room_activity(Rocket_passageres.get(passager));
+    }
+
+	private Rocket initialize() {
 		System.out.println();
 		System.out.println("Генерируем карту:");
 
-		//create space town with random people
-		town = new Space_town();
-
-		//check, that people in town > 0 else exeption
-		int last_men;
-		try {
-			last_men = town.civil_count() - 1;
-			if (last_men == -1) throw new SilentHill(town.toString());
-			Space_town.town_civilian x = town.getpeople(last_men);
-			System.out.println("Номер " + x.getNumber() + " знал что в ракете что-то происходит");
+		try
+		{
+			get_indofmation_about_rocket_in_town(new Space_town());
 		} catch (SilentHill e)
 		{
 			System.out.println(e);
 			System.out.println("Никто тут не живёт, город пуст");
-		}
-
-	System.out.println();
-
-	for (int i = 0; i < room_count; i++)
-	{
-		//Random next room count (0..2) and create this rooms
-		long next_room_count = Math.round(Math.random()*2);
-		Room RandomRoom1,RandomRoom2;
-		if (next_room_count == 0)
-			RandomRoom1 = RandomRoom2 = null;
-		else if (next_room_count == 1){
-			RandomRoom1 = getRandomRoom(Rooms); RandomRoom2 = null;
-		} else {
-			RandomRoom1 = getRandomRoom(Rooms);
-			RandomRoom2 = getRandomRoom(Rooms);
-		}
-
-		//create Random room
-		double chance = Math.random();
-		if (chance < 0.2)
-			Rooms.add(new Lift("Лифт "+i, RandomRoom1, RandomRoom2, Math.random()));
-		else
-			Rooms.add(new Room("Комната "+i,RandomRoom1, RandomRoom2, Math.random()) );
-	}
-
 			System.out.println();
-	System.out.println("Готовим героев:");
+		}
 
-	//create Random count passanger with random location, knowledge and status
-	for (int i = 0; i < passager_number; i++)
-	{
-		RocketKnowledge knowledge = (Math.random() > 0.5) ? RocketKnowledge.BAD : RocketKnowledge.GOOD;
-		Status stat = (Math.random() > 0.3) ? Status.AWAKE : Status.SLEEP;
-		int location = (int)Math.round(Math.random() * (room_count-1) );
-		Room location_this = Rooms.get(location);
-		Rocket_passageres.add(new Rocket_passager(String.valueOf(i), knowledge ,stat, location_this) );
+		create_rooms();
+		create_heroes();
+		create_devices();
+		System.out.printf("Иницилизация закончена.\n\n");
+		Interactive_mode.writer_passageres(Rocket_passageres);
+		return new Rocket(Rocket_passageres, Rooms, Devices);
 	}
 
 
-	System.out.println("Еще немного устройтв:");
+	private Rocket initialize(String path_to_passageres, String path_to_romms) {
+		System.out.println();
+		System.out.println("Генерируем карту:");
 
-	//Create random device count
-	for (int i = 0; i < device_count; i++)
+		try
+		{
+			get_indofmation_about_rocket_in_town(new Space_town());
+		} catch (SilentHill e)
+		{
+			System.out.println(e);
+			System.out.println("Никто тут не живёт, город пуст");
+			System.out.println();
+		}
+
+		Room_list rooms = Interactive_mode.read_from_xml(Room_list.class, path_to_romms);
+		Rooms = rooms.getRooms();
+		room_count = Rooms.size();
+		for(Room room : Rooms)
+			for(Room nextroom : Rooms) {
+				if ((room.getNext_room1() != null)&&(room.getNext_room1().getName().equals(nextroom.getName()))) {
+						room.setNext_room1(nextroom);
+					}
+				if ((room.getNext_room2() != null) && (room.getNext_room2().getName().equals(nextroom.getName()))) {
+					room.setNext_room2(nextroom);
+				}
+			}
+
+		Rocket rocket = Interactive_mode.read_from_xml(Rocket.class, path_to_passageres);
+
+		Rocket_passageres = rocket.getRocket_passageres();
+		for(Rocket_passager passager : Rocket_passageres)
+			for(Room room : Rooms)
+				if (room.getName().equals(passager.getPlace().getName()))
+					passager.setPlace(room);
+
+		passager_number = Rocket_passageres.size();
+
+		create_devices();
+		System.out.printf("Иницилизация закончена.\n\n");
+		return new Rocket(Rocket_passageres, Rooms, Devices);
+	}
+
+	static void get_indofmation_about_rocket_in_town( Space_town town) throws SilentHill
 	{
-		//Create Random Device in random location
+        int last_men = town.civil_count() - 1;
+		if (last_men == -1) throw new SilentHill(town.toString());
+
+		Space_town.town_civilian civilian = town.getpeople(last_men);
+		System.out.println("Номер " + civilian.getNumber() + " знал что в ракете что-то происходит");
+
+		System.out.println();
+	}
+
+	private static void create_rooms()
+	{
+		for (int room_number = 0; room_number < room_count; room_number++)
+		{
+			Room [] Next_rooms = generate_next_rooms();
+			Rooms.add(init_room(room_number, Next_rooms[0], Next_rooms[1]));
+		}
+		System.out.println();
+	}
+
+	private static Room [] generate_next_rooms()
+	{
+		Room [] Next_rooms = new Room[2];
+		Random next_rooms = new Random();
+		switch (next_rooms.nextInt(4))
+		{
+			case 0:
+				Next_rooms[0] = Next_rooms[1] = null;
+				break;
+			case 1:
+				Next_rooms[0] = getRandomRoom(Rooms); Next_rooms[1] = null;
+				break;
+			default:
+				Next_rooms[0] = getRandomRoom(Rooms);
+				Next_rooms[1] = getRandomRoom(Rooms);
+		}
+		return Next_rooms;
+	}
+
+	private static Room init_room(int room_number, Room NextRoom1, Room NextRoom2)
+	{
+		return Math.random() < 0.2 ? new Lift("Лифт "+room_number, NextRoom1, NextRoom2, Math.random()) :
+				new Room("Комната "+room_number,NextRoom1, NextRoom2, Math.random());
+	}
+
+	private static void create_heroes()
+	{
+		System.out.println("Готовим героев:");
+		//create Random count passanger with random location, knowledge and status
+		for (int i = 0; i < passager_number; i++)
+		{
+			RocketKnowledge knowledge = (Math.random() > 0.5) ? RocketKnowledge.BAD : RocketKnowledge.GOOD;
+			Status stat = (Math.random() > 0.3) ? Status.AWAKE : Status.SLEEP;
+			int location = new Random().nextInt((int)(room_count-1));
+			Room location_this = Rooms.get(location);
+			Rocket_passageres.add(new Rocket_passager(String.valueOf(i), knowledge ,stat, location_this) );
+		}
+		System.out.println();
+	}
+
+	private static void create_devices()
+	{
+		System.out.println("Еще немного устройтв:");
+
+		//Create random device count
+		for (int device_number = 0; device_number < device_count; device_number++)
+		{
+			//Create Random Device in random location
+			Devices.add(get_device(device_number));
+			//each Divese have Bottom in Random Room
+			getRandomRoom(Rooms).addBottom(new Bottom(Devices.get(device_number)));
+		}
+	}
+
+	private static Device get_device(int device_number)
+	{
 		double chance = Math.random();
 		if (chance < 0.4)
-			Devices.add(new Device("Устройство " + i, getRandomRoom(Rooms)));
-		 else if (chance < 0.6)
-			Devices.add(new Rocket_engine("Двигатель " + i, getRandomRoom(Rooms)));
-		 else if (chance < 0.8)
-			Devices.add(new Gravity_device("Прибор невесомости " + i, getRandomRoom(Rooms)));
-		 else
-			Devices.add(new Gravity_device("Выключатель света " + i, getRandomRoom(Rooms)));
-
-		//each Divese have Bottom in Random Room
-		getRandomRoom(Rooms).addBottom(new Bottom(Devices.get(i)));
-	}
-	Rockkket = new Rocket(Rocket_passageres, Rooms, Devices);
-
-	System.out.printf("Иницилизация закончена.\n\n");
+			return(new Device("Устройство " + device_number, getRandomRoom(Rooms)));
+		else if (chance < 0.6)
+			return(new Rocket_engine("Двигатель " + device_number, getRandomRoom(Rooms)));
+		else if (chance < 0.8)
+			return(new Gravity_device("Прибор невесомости " + device_number, getRandomRoom(Rooms)));
+		else
+			return(new Gravity_device("Выключатель света " + device_number, getRandomRoom(Rooms)));
 	}
 
-	static private Room getRandomRoom(ArrayList<Room> rooms)
+	static private Room getRandomRoom(LinkedList<Room> rooms)
 	{
 		if (rooms.size() < 1 ) return null;
 		int i = (int)Math.round (Math.random() * (rooms.size()-1) );
