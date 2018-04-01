082f989a83c4cf5754b8d514dc74e90af60be0d7
Igor Anokhin <igor.anohin@emlid.com>
2018-04-01 15:34:56 +0300

    Human: add create time and size

--- Human.java
+++ Human.java
@@ -1,21 +1,39 @@
+import java.time.LocalTime;
+
 public class Human extends Animal
 {
+	protected Size size;
 	protected Room place;
-	Human(){}
+	protected LocalTime create_time;
+
+	Human(){
+		this.size = Size.NORMAL;
+		create_time = java.time.LocalTime.now();
+	}
 
 	Human(String name, Status status, Room place)
 	{
 		this.place = place;
 		this.name = name;
 		this.status = status;
+		this.size = Size.NORMAL;
+		create_time = java.time.LocalTime.now();
 		System.out.printf("Персонаж: \"%s\" создан\n", name);
+		System.out.println("Его размер " + size);
+		System.out.println("Время создания: ");
+		System.out.println();
 	}
 
 	Human(String name, Status status)
 	{
 		this.name = name;
 		this.status = status;
+		this.size = Size.NORMAL;
+		create_time = java.time.LocalTime.now();
 		System.out.printf("Персонаж: \"%s\" создан\n", name);
+		System.out.println("Его размер " + size);
+		System.out.println("Время создания: ");
+		System.out.println();
 	}
 	public String getName()
 	{
@@ -40,6 +58,9 @@ public class Human extends Animal
 	{
 		this.place = place;
 	}
+
+	public Size getSize() {return size;}
+	public LocalTime getCreate_time() {return create_time;}
 
 }
 
