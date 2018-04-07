ec831612059f3c290215f73cc1dfdc6dbb57abc6
Igor Anokhin <igor.anohin@emlid.com>
2018-04-08 02:49:07 +0300

    Client for Servere

---
+++ Client.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Main {
+    private static String end_messages = "It's all.";
+
+    public static void main(String[] args) throws IOException {
+
+        String hostName = "127.0.0.1";
+        int portNumber = 8080;
+
+        try (
+                Socket echoSocket = new Socket(hostName, portNumber);
+                PrintWriter out =
+                        new PrintWriter(echoSocket.getOutputStream(), true);
+                DataInputStream ois = new DataInputStream(echoSocket.getInputStream())
+
+        ) {
+            String userInput;
+            Scanner input = new Scanner(System.in);
+            System.out.println("HERE1");
+            //ObjectOutputStream outobjects = new ObjectOutputStream(echoSocket.getOutputStream());
+            //ObjectInputStream inobjects = new ObjectInputStream(echoSocket.getInputStream());
+            System.out.println("HERE");
+
+
+            while (true) {
+                userInput = input.nextLine();
+                out.println(userInput);
+                out.flush();
+                String line = "";
+                byte[] qwe = new byte[100];
+                int bytesRead = 0;
+                do {
+                        bytesRead = ois.read(qwe);
+                        //bytesRead = objects.read(qwe);
+                        String server_message = new String(qwe, 0, bytesRead);
+                        line += server_message;
+                } while (!line.endsWith(end_messages));
+                System.out.println(line);
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                    hostName);
+            System.exit(1);
+        }
+    }
+}
+
