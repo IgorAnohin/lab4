245b0621b59b930a30062c083608be4ed1afb420
Igor Anokhin <igor.anohin@emlid.com>
2018-06-04 09:23:56 +0300

    GUI_SERVER: delete useless imports

--- GUI_SERVER.java
+++ GUI_SERVER.java
@@ -1,21 +1,3 @@
-import sun.nio.cs.ext.EUC_JP_LINUX;
-
-import javax.swing.*;
-import javax.swing.table.AbstractTableModel;
-import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.text.Collator;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class GUI_SERVER {
     public static void main(String[] args) {
@@ -23,11 +5,14 @@ public class GUI_SERVER {
 
         try {
             MainWindow mainWindow = new MainWindow();
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            SwingUtilities.updateComponentTreeUI(mainWindow);
+            //UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+            //SwingUtilities.updateComponentTreeUI(mainWindow);
             //GameWindow window = new GameWindow();
         }catch (Exception e) {
+            System.out.println(e);
             System.out.println("FAILED");
         }
     }
 }
+//язык сохранить
