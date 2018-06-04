fe0bb631c47c4e76bee635505d29f6b717a6726e
Igor Anokhin <igor.anohin@emlid.com>
2018-06-04 09:26:33 +0300

    MainWindow: add languages, DB and class for control readwrite

--- MainWindow.java
+++ MainWindow.java
@@ -1,22 +1,48 @@
+import sun.nio.cs.ext.EUC_JP_LINUX;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.*;
+import java.sql.SQLException;
+
+
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.util.Locale;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
 
 class MainWindow extends JFrame {
 
     final static int WIDTH_W = 300;
     final static int HEIGHT_W = 300;
 
-    JLabel log_and_pass_label = new JLabel("Please, enter your login and password");
+    JLabel log_and_pass_label;
 
-    JLabel log_lab = new JLabel("     Login:");
+    JLabel log_lab;
     JTextField log_field = new JTextField("");
 
-    JLabel pas_lab = new JLabel("     Password:");
+    JLabel pas_lab;
     JPasswordField passwordField = new JPasswordField();
 
     JButton checkButton = new JButton();
+    JButton langButton = new JButton();
+
+    String defaultLan = "el";
+    ResourceBundle r;
 
     public MainWindow() {
         this.pack();
@@ -28,14 +54,27 @@ class MainWindow extends JFrame {
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
 
+
+
+
+
+        Locale dLocale = new Locale.Builder().setLanguage(defaultLan).build();
+        r = ResourceBundle.getBundle("languages", dLocale, new UTF8Control());
+        log_and_pass_label  = new JLabel("  " + r.getString("info_mes"));
+        log_lab =  new JLabel("     " + r.getString("log_lab"));
+        pas_lab  = new JLabel("     " + r.getString("pas_lab"));
+        checkButton.setText(r.getString("next_but"));
+        langButton.setText(r.getString("lang_but"));
+
         JPanel panel= new JPanel();
-        panel.setLayout(new GridLayout(4,1));
+        panel.setLayout(new GridLayout(5,1));
+        panel.add(langButton);
+        addChangeButtonListener();
         panel.add(log_and_pass_label);
 
         addLoginFileds(panel);
         addPasswordFields(panel);
 
-        checkButton.setText("Next");
         panel.add(checkButton);
         addCheckButtonInput();
         this.setContentPane(panel);
@@ -69,17 +108,119 @@ class MainWindow extends JFrame {
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
 
-                String password = "ololo";
                 String enteredPassword = new String(passwordField.getPassword());
 
-                if (password.equals(enteredPassword) && log_field.getText().equals("igor")){
-                    MainWindow.super.setVisible(false);
-                    GameWindow window = new GameWindow();
-                } else {
-                    checkButton.setText("YOU ARE BAD BOY");
+
+                String url = "jdbc:postgresql://localhost/test_db";
+                String user = "postgres";
+                String passw = "football";
+
+                boolean success = false;
+
+                try {
+                    Class.forName("org.postgresql.Driver");
+
+                    MessageDigest digest = MessageDigest.getInstance("SHA-256");
+                    byte[] hash = digest.digest(enteredPassword.getBytes(StandardCharsets.UTF_8));
+                    Integer sum = 0;
+                    for (byte bb : hash) {
+                        sum += bb;
+                    }
+
+                    System.out.println(sum);
+                    try (Connection connection = DriverManager.getConnection(url, user, passw)) {
+                        PreparedStatement st = connection.prepareStatement("select * from LOL");
+                        ResultSet result = st.executeQuery();
+
+                        while (result.next()) {
+
+
+                            if (sum.toString().equals(result.getString(3)) && log_field.getText().equals(result.getString(2))) {
+                                MainWindow.super.setVisible(false);
+
+                                if (result.getString(4) == null) {
+                                    System.out.println("NULL");
+                                    try {
+                                        PreparedStatement st2 = connection.prepareStatement("UPDATE lol set LANGUAGE ='" + defaultLan + "' where id='" + result.getString(1) + "';");
+                                        ResultSet result2 = st2.executeQuery();
+                                    } catch (Exception eee) {}
+                                    System.out.println(defaultLan);
+                                } else {
+                                    defaultLan = result.getString(4);
+                                    System.out.println(defaultLan);
+                                }
+                                GameWindow window = new GameWindow(defaultLan);
+                                break;
+                            }
+                        }
+                    }
+                } catch (Exception ee) {
+                    System.out.println("BAD");
                 }
+
+                if (!success)
+                    checkButton.setText(r.getString("Bad_Boy"));
 
             }
         });
+    }
+
+    void addChangeButtonListener() {
+
+        langButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+
+                if (defaultLan.equals("ru"))
+                    defaultLan = "en";
+                else if (defaultLan.equals("en"))
+                    defaultLan = "el";
+                else
+                    defaultLan = "ru";
+
+                Locale dLocale = new Locale.Builder().setLanguage(defaultLan).build();
+                r = ResourceBundle.getBundle("languages", dLocale, new UTF8Control());
+                log_and_pass_label.setText("  " + r.getString("info_mes"));
+                log_lab.setText("     " + r.getString("log_lab"));
+                pas_lab.setText("     " + r.getString("pas_lab"));
+                checkButton.setText(r.getString("next_but"));
+                langButton.setText(r.getString("lang_but"));
+            }
+        });
+    }
+}
+
+class UTF8Control extends ResourceBundle.Control {
+    public ResourceBundle newBundle
+            (String baseName, Locale locale, String format, ClassLoader loader, boolean reload)
+            throws IllegalAccessException, InstantiationException, IOException
+    {
+        // The below is a copy of the default implementation.
+        String bundleName = toBundleName(baseName, locale);
+        String resourceName = toResourceName(bundleName, "properties");
+        ResourceBundle bundle = null;
+        InputStream stream = null;
+        if (reload) {
+            URL url = loader.getResource(resourceName);
+            if (url != null) {
+                URLConnection connection = url.openConnection();
+                if (connection != null) {
+                    connection.setUseCaches(false);
+                    stream = connection.getInputStream();
+                }
+            }
+        } else {
+            stream = loader.getResourceAsStream(resourceName);
+        }
+        if (stream != null) {
+            try {
+                // Only this line is changed to make it to read properties files as UTF-8.
+                bundle = new PropertyResourceBundle(new InputStreamReader(stream, "UTF-8"));
+            } finally {
+                stream.close();
+            }
+        }
+        return bundle;
     }
 }
